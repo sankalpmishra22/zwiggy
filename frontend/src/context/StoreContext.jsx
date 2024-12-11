447814008e41371c8f571c889b27e765b7da8664
sankalpmishra22 <sankalp1606@gmail.com>
2024-12-11 21:27:52 +0530

    Login & Register functionality completed

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,6 +4,9 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) =>{
  const [cartItems,setCartItems] = useState({});
+ const url = "http://localhost:4000"
+ const [token,setToken] = useState("");
+
  const addToCart = (itemId) =>{
     if(!cartItems[itemId]){
         setCartItems((prev)=>({...prev,[itemId]:1}))
@@ -31,13 +34,23 @@ const StoreContextProvider = (props) =>{
     }
     return totalAmount;
  }
+
+    useEffect(()=>{
+        if(localStorage.getItem("token")){
+            setToken(localStorage.getItem("token"))
+        }
+    },[])
+
     const contextValue ={
         food_list,
         cartItems,
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
 
     return (
