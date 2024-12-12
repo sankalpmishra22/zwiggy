603a846dc30e2b67eac68a24d71da42f4be9c55d
sankalpmishra22 <sankalp1606@gmail.com>
2024-12-13 00:09:53 +0530

    Created User Order Page

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,4 +1,5 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 import axios from 'axios';
@@ -47,8 +48,18 @@ const PlaceOrder = () => {
     else{
       alert("Error");
     }
-    
   }
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(!token){
+      navigate('/cart')
+    }
+    else if(getTotalCartAmount()===0){
+      navigate('/cart')
+    }
+  },[token])
 
   return (
    <form onSubmit={placeOrder} className='place-order'>
